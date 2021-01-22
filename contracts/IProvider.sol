1c76a3396fc1f31c14472ee6ba8cface464e12f6
Edgarillo <67366139+Edgarillo@users.noreply.github.com>
2021-01-22 16:56:07 +0100

    Changes
    Changes on provider Aave

--- contracts/IProvider.sol
+++ contracts/IProvider.sol
@@ -3,8 +3,10 @@
 pragma solidity >=0.4.25 <0.7.0;
 
 interface IProvider {
-  // TODO
 
-  function deposit(address collateralAsset, uint256 collateralAmount) external virtual payable;
-  function borrow(address borrowAsset, uint256 borrowAmount) external virtual payable;
+function deposit(address collateralAsset, uint256 collateralAmount) external virtual payable;
+function borrow(address borrowAsset, uint256 borrowAmount) external virtual payable;
+function withdraw(address collateralAsset, uint256 collateralAmount) external virtual payable;
+function payback(address borrowAsset, uint256 borrowAmount) external virtual payable;
+
 }
--- contracts/ProviderAave.sol
+++ contracts/ProviderAave.sol
@@ -33,7 +33,6 @@ interface AaveLendingPoolProviderInterface {
     function getLendingPool() external view returns (address);
 }
 
-// Aave Protocol Data Provider
 interface AaveDataProviderInterface {
     function getReserveTokensAddresses(address _asset) external view returns (
         address aTokenAddress,
@@ -65,33 +64,28 @@ contract ProviderAave is IProvider {
   using SafeMath for uint256;
   using UniERC20 for IERC20;
 
-  /**
-     * @dev get Aave Lending Pool Provider
-    */
+    function sub(uint x, uint y) internal virtual pure returns (uint z) {
+    z = SafeMath.sub(x, y);
+    }
+
     function getAaveProvider() internal pure returns (AaveLendingPoolProviderInterface) {
         return AaveLendingPoolProviderInterface(0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5); //mainnet
-        // return AaveLendingPoolProviderInterface(0x652B2937Efd0B5beA1c8d54293FC1289672AFC6b); //kovan
     }
 
-    /**
-     * @dev get Aave Protocol Data Provider
-    */
     function getAaveDataProvider() internal pure returns (AaveDataProviderInterface) {
         return AaveDataProviderInterface(0x057835Ad21a177dbdd3090bB1CAE03EaCF78Fc6d); //mainnet
-        // return AaveDataProviderInterface(0x744C1aaA95232EeF8A9994C4E0b3a89659D9AB79); //kovan
     }
 
-    /**
-     * @dev Return Weth address
-    */
+    function getPaybackBalance(AaveDataProviderInterface aaveData, address token, uint rateMode) internal view returns (uint) {
+        (, uint stableDebt, uint variableDebt, , , , , , ) = aaveData.getUserReserveData(token, address(this));
+        return rateMode == 1 ? stableDebt : variableDebt;
+    }
+
     function getWethAddr() internal pure returns (address) {
         return 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2; // Mainnet WETH Address
-        // return 0xd0A1E359811322d97991E03f863a0C30C2cF029C; // Kovan WETH Address
     }
-      /**
-   * @dev Return ethereum address
-   */
-  function getEthAddr() internal pure returns (address) {
+
+    function getEthAddr() internal pure returns (address) {
     return 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE; // ETH Address
   }
 
@@ -137,6 +131,8 @@ contract ProviderAave is IProvider {
         }
 
     }
+
+
       function borrow(address borrowAsset, uint256 borrowAmount) external override payable {
 
         AaveInterface aave = AaveInterface(getAaveProvider().getLendingPool());
@@ -146,6 +142,48 @@ contract ProviderAave is IProvider {
 
         aave.borrow(_token, borrowAmount, 2, 0, address(this));
         convertWethToEth(isEth, TokenInterface(_token), borrowAmount);
+
+    }
+
+
+      function withdraw(address collateralAsset, uint256 collateralAmount) external override payable {
+
+        AaveInterface aave = AaveInterface(getAaveProvider().getLendingPool());
+
+        bool isEth = collateralAsset == getEthAddr();
+        address _token = isEth ? getWethAddr() : collateralAsset;
+
+        TokenInterface tokenContract = TokenInterface(_token);
+
+        uint initialBal = tokenContract.balanceOf(address(this));
+        aave.withdraw(_token, collateralAmount, address(this));
+        uint finalBal = tokenContract.balanceOf(address(this));
+
+        collateralAmount = sub(finalBal, initialBal);
+
+        convertWethToEth(isEth, tokenContract, collateralAmount);
+
+    }
+
+
+
+      function payback(address borrowAsset, uint256 borrowAmount) external override payable {
+
+        AaveInterface aave = AaveInterface(getAaveProvider().getLendingPool());
+        AaveDataProviderInterface aaveData = getAaveDataProvider();
+
+        bool isEth = borrowAsset == getEthAddr();
+        address _token = isEth ? getWethAddr() : borrowAsset;
+
+        TokenInterface tokenContract = TokenInterface(_token);
+
+        borrowAmount = borrowAmount == uint(-1) ? getPaybackBalance(aaveData, _token, 2) : borrowAmount;
+
+        if (isEth) convertEthToWeth(isEth, tokenContract, borrowAmount);
+
+        tokenContract.approve(address(aave), borrowAmount);
+
+        aave.repay(_token, borrowAmount, 2, address(this));
 
     }
 

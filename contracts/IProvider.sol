961d2133538aa4da92f0c279fdd97987c5b01c07
Boyan Barakov <boyan.barakov@gmail.com>
2021-01-23 19:14:50 +0100

    add signatures for two more functions in IProvider

--- contracts/IProvider.sol
+++ contracts/IProvider.sol
@@ -9,4 +9,12 @@ interface IProvider {
   function withdraw(address collateralAsset, uint256 collateralAmount) external payable;
   function payback(address borrowAsset, uint256 borrowAmount) external payable;
 
+  // returns the address of the assets received from depositing a collateral;
+  // for ex. for ETH as collateralAsset, AaveProvider should return the address of aWETH
+  // and Compound should return the address of cETH. 
+  //function getRedeemableAddress(address collateralAsset) external returns(address);
+
+  // returns the borrow annualized rate for an asset
+  //function getBorrowRateFor(address asset) external returns(uint256);
+
 }

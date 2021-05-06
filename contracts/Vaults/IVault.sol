dec27b876376c094ad66443011a87d0b7069f5d8
Boyan Barakov <boyan.barakov@gmail.com>
2021-05-06 19:10:21 +0200

    lint Vaults/IVault.sol

--- contracts/Vaults/IVault.sol
+++ contracts/Vaults/IVault.sol
@@ -4,7 +4,6 @@ pragma solidity >=0.6.12;
 pragma experimental ABIEncoderV2;
 
 interface IVault {
-
   // Events
 
   // Log Users Deposit
@@ -19,29 +18,52 @@ interface IVault {
   // Log New active provider
   event SetActiveProvider(address providerAddr);
   // Log Switch providers
-  event Switch(address vault, address fromProviderAddrs, address toProviderAddr, uint256 Debtamount, uint256 Collattamount);
+  event Switch(
+    address vault,
+    address fromProviderAddrs,
+    address toProviderAddr,
+    uint256 Debtamount,
+    uint256 Collattamount
+  );
 
   // Core Vault Functions
 
   function deposit(uint256 _collateralAmount) external payable;
+
   function withdraw(int256 _withdrawAmount) external;
+
   function borrow(uint256 _borrowAmount) external;
+
   function payback(int256 _repayAmount) external payable;
-  function executeSwitch(address _newProvider,uint256 _flashLoanDebt, uint256 fee) external;
+
+  function executeSwitch(
+    address _newProvider,
+    uint256 _flashLoanDebt,
+    uint256 fee
+  ) external;
 
   //Getter Functions
 
-  function activeProvider() external view returns(address);
-  function borrowBalance(address _provider) external view returns(uint256);
-  function depositBalance(address _provider) external view returns(uint256);
-  function getNeededCollateralFor(uint256 _amount, bool _withFactors) external view returns(uint256);
-  function getLiquidationBonusFor(uint256 _amount,bool _flash) external view returns(uint256);
-  function getProviders() external view returns(address[] memory);
-  function fujiERC1155() external view returns(address);
+  function activeProvider() external view returns (address);
+
+  function borrowBalance(address _provider) external view returns (uint256);
+
+  function depositBalance(address _provider) external view returns (uint256);
+
+  function getNeededCollateralFor(uint256 _amount, bool _withFactors)
+    external
+    view
+    returns (uint256);
+
+  function getLiquidationBonusFor(uint256 _amount, bool _flash) external view returns (uint256);
+
+  function getProviders() external view returns (address[] memory);
+
+  function fujiERC1155() external view returns (address);
 
   //Setter Functions
 
   function setActiveProvider(address _provider) external;
-  function updateF1155Balances() external;
 
+  function updateF1155Balances() external;
 }

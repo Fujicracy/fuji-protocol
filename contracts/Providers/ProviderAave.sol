d0d4d411aa04c06dd365faf45b102784c234a538
Boyan Barakov <boyan.barakov@gmail.com>
2021-05-06 17:10:36 +0200

    lint Providers/ProviderAave.sol

--- contracts/Providers/ProviderAave.sol
+++ contracts/Providers/ProviderAave.sol
@@ -8,19 +8,40 @@ import { UniERC20 } from "../Libraries/LibUniERC20.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IProvider } from "./IProvider.sol";
 
-interface TokenInterface {
+interface ITokenInterface {
   function approve(address, uint256) external;
-  function transfer(address, uint) external;
-  function transferFrom(address, address, uint) external;
+
+  function transfer(address, uint256) external;
+
+  function transferFrom(
+    address,
+    address,
+    uint256
+  ) external;
+
   function deposit() external payable;
-  function withdraw(uint) external;
-  function balanceOf(address) external view returns (uint);
-  function decimals() external view returns (uint);
+
+  function withdraw(uint256) external;
+
+  function balanceOf(address) external view returns (uint256);
+
+  function decimals() external view returns (uint256);
 }
 
-interface AaveInterface {
-  function deposit(address _asset, uint256 _amount, address _onBehalfOf, uint16 _referralCode) external;
-  function withdraw(address _asset, uint256 _amount, address _to) external;
+interface IAaveInterface {
+  function deposit(
+    address _asset,
+    uint256 _amount,
+    address _onBehalfOf,
+    uint16 _referralCode
+  ) external;
+
+  function withdraw(
+    address _asset,
+    uint256 _amount,
+    address _to
+  ) external;
+
   function borrow(
     address _asset,
     uint256 _amount,
@@ -28,7 +49,14 @@ interface AaveInterface {
     uint16 _referralCode,
     address _onBehalfOf
   ) external;
-  function repay(address _asset, uint256 _amount, uint256 _rateMode, address _onBehalfOf) external;
+
+  function repay(
+    address _asset,
+    uint256 _amount,
+    uint256 _rateMode,
+    address _onBehalfOf
+  ) external;
+
   function setUserUseReserveAsCollateral(address _asset, bool _useAsCollateral) external;
 }
 
@@ -37,217 +65,231 @@ interface AaveLendingPoolProviderInterface {
 }
 
 interface AaveDataProviderInterface {
-  function getReserveTokensAddresses(address _asset) external view returns (
-    address aTokenAddress,
-    address stableDebtTokenAddress,
-    address variableDebtTokenAddress
-  );
-  function getUserReserveData(address _asset, address _user) external view returns (
-    uint256 currentATokenBalance,
-    uint256 currentStableDebt,
-    uint256 currentVariableDebt,
-    uint256 principalStableDebt,
-    uint256 scaledVariableDebt,
-    uint256 stableBorrowRate,
-    uint256 liquidityRate,
-    uint40 stableRateLastUpdated,
-    bool usageAsCollateralEnabled
-  );
-  function getReserveData(address asset) external view returns (
-    uint256 availableLiquidity,
-    uint256 totalStableDebt,
-    uint256 totalVariableDebt,
-    uint256 liquidityRate,
-    uint256 variableBorrowRate,
-    uint256 stableBorrowRate,
-    uint256 averageStableBorrowRate,
-    uint256 liquidityIndex,
-    uint256 variableBorrowIndex,
-    uint40 lastUpdateTimestamp
-  );
+  function getReserveTokensAddresses(address _asset)
+    external
+    view
+    returns (
+      address aTokenAddress,
+      address stableDebtTokenAddress,
+      address variableDebtTokenAddress
+    );
+
+  function getUserReserveData(address _asset, address _user)
+    external
+    view
+    returns (
+      uint256 currentATokenBalance,
+      uint256 currentStableDebt,
+      uint256 currentVariableDebt,
+      uint256 principalStableDebt,
+      uint256 scaledVariableDebt,
+      uint256 stableBorrowRate,
+      uint256 liquidityRate,
+      uint40 stableRateLastUpdated,
+      bool usageAsCollateralEnabled
+    );
+
+  function getReserveData(address _asset)
+    external
+    view
+    returns (
+      uint256 availableLiquidity,
+      uint256 totalStableDebt,
+      uint256 totalVariableDebt,
+      uint256 liquidityRate,
+      uint256 variableBorrowRate,
+      uint256 stableBorrowRate,
+      uint256 averageStableBorrowRate,
+      uint256 liquidityIndex,
+      uint256 variableBorrowIndex,
+      uint40 lastUpdateTimestamp
+    );
 }
 
 interface AaveAddressProviderRegistryInterface {
   function getAddressesProvidersList() external view returns (address[] memory);
 }
 
-interface ATokenInterface {
-  function balanceOf(address _user) external view returns(uint256);
-}
-
 contract ProviderAave is IProvider {
   using SafeMath for uint256;
   using UniERC20 for IERC20;
 
-  function getAaveProvider() internal pure returns (AaveLendingPoolProviderInterface) {
+  function _getAaveProvider() internal pure returns (AaveLendingPoolProviderInterface) {
     return AaveLendingPoolProviderInterface(0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5); //mainnet
   }
 
-  function getAaveDataProvider() internal pure returns (AaveDataProviderInterface) {
+  function _getAaveDataProvider() internal pure returns (AaveDataProviderInterface) {
     return AaveDataProviderInterface(0x057835Ad21a177dbdd3090bB1CAE03EaCF78Fc6d); //mainnet
   }
 
-  function getWethAddr() internal pure returns (address) {
+  function _getWethAddr() internal pure returns (address) {
     return 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2; // Mainnet WETH Address
   }
 
-  function getEthAddr() internal pure returns (address) {
+  function _getEthAddr() internal pure returns (address) {
     return 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE; // ETH Address
   }
 
-  function getIsColl(AaveDataProviderInterface aaveData, address token, address user) internal view returns (bool isCol) {
-    (, , , , , , , , isCol) = aaveData.getUserReserveData(token, user);
+  function _getIsColl(
+    AaveDataProviderInterface _aaveData,
+    address _token,
+    address _user
+  ) internal view returns (bool isCol) {
+    (, , , , , , , , isCol) = _aaveData.getUserReserveData(_token, _user);
   }
 
-  function convertEthToWeth(bool isEth, TokenInterface token, uint amount) internal {
-    if(isEth) token.deposit{value : amount }();
+  function _convertEthToWeth(
+    bool _isEth,
+    ITokenInterface _token,
+    uint256 _amount
+  ) internal {
+    if (_isEth) _token.deposit{ value: _amount }();
   }
 
-  function convertWethToEth(bool isEth, TokenInterface token, uint amount) internal {
-    if(isEth) {
-      token.approve(address(token), amount);
-      token.withdraw(amount);
+  function convertWethToEth(
+    bool _isEth,
+    ITokenInterface _token,
+    uint256 _amount
+  ) internal {
+    if (_isEth) {
+      _token.approve(address(_token), _amount);
+      _token.withdraw(_amount);
     }
   }
-      /**
-     * @dev Return the borrowing rate of ETH/ERC20_Token.
-     * @param asset to query the borrowing rate.
-    */
-  function getBorrowRateFor(address asset) external view override returns(uint256) {
 
-    AaveDataProviderInterface aaveData = getAaveDataProvider();
+  /**
+   * @dev Return the borrowing rate of ETH/ERC20_Token.
+   * @param _asset to query the borrowing rate.
+   */
+  function getBorrowRateFor(address _asset) external view override returns (uint256) {
+    AaveDataProviderInterface aaveData = _getAaveDataProvider();
 
-    bool isEth = asset == getEthAddr();
-    address botoken = isEth ? getWethAddr() : asset;
-
-    ( , , , ,uint256 variableBorrowRate, , , , ,) = AaveDataProviderInterface(aaveData).getReserveData(botoken);
+    (, , , , uint256 variableBorrowRate, , , , , ) =
+      AaveDataProviderInterface(aaveData).getReserveData(
+        _asset == _getEthAddr() ? _getWethAddr() : _asset
+      );
 
     return variableBorrowRate;
   }
 
-      /**
-     * @dev Return borrow balance of ETH/ERC20_Token.
-     * @param borrowAsset token address to query the balance.
-    */
-  function getBorrowBalance(address borrowAsset) external view override returns(uint256) {
-    AaveDataProviderInterface aaveData = getAaveDataProvider();
+  /**
+   * @dev Return borrow balance of ETH/ERC20_Token.
+   * @param _asset token address to query the balance.
+   */
+  function getBorrowBalance(address _asset) external view override returns (uint256) {
+    AaveDataProviderInterface aaveData = _getAaveDataProvider();
 
-    bool isEth = borrowAsset == getEthAddr();
-    address _token = isEth ? getWethAddr() : borrowAsset;
+    bool isEth = _asset == _getEthAddr();
+    address _token = isEth ? _getWethAddr() : _asset;
 
-    (,, uint variableDebt, , , , , , ) = aaveData.getUserReserveData(_token, msg.sender);
+    (, , uint256 variableDebt, , , , , , ) = aaveData.getUserReserveData(_token, msg.sender);
 
     return variableDebt;
   }
 
-      /**
-     * @dev Return deposit balance of ETH/ERC20_Token.
-     * @param collateralAsset token address to query the balance.
-    */
-    function getDepositBalance(address collateralAsset) external view override returns(uint256) {
-    AaveDataProviderInterface aaveData = getAaveDataProvider();
+  /**
+   * @dev Return deposit balance of ETH/ERC20_Token.
+   * @param _asset token address to query the balance.
+   */
+  function getDepositBalance(address _asset) external view override returns (uint256) {
+    AaveDataProviderInterface aaveData = _getAaveDataProvider();
 
-    bool isEth = collateralAsset == getEthAddr();
-    address _token = isEth ? getWethAddr() : collateralAsset;
+    bool isEth = _asset == _getEthAddr();
+    address _token = isEth ? _getWethAddr() : _asset;
 
-    (uint256 atokenBal,, , , , , , , ) = aaveData.getUserReserveData(_token, msg.sender);
+    (uint256 atokenBal, , , , , , , , ) = aaveData.getUserReserveData(_token, msg.sender);
 
     return atokenBal;
-    }
+  }
 
-      /**
-     * @dev Deposit ETH/ERC20_Token.
-     * @param collateralAsset token address to deposit.(For ETH: 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE)
-     * @param collateralAmount token amount to deposit.
-    */
-  function deposit(address collateralAsset, uint256 collateralAmount) external override payable {
+  /**
+   * @dev Deposit ETH/ERC20_Token.
+   * @param _asset token address to deposit.(For ETH: 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE)
+   * @param _amount token amount to deposit.
+   */
+  function deposit(address _asset, uint256 _amount) external payable override {
+    IAaveInterface aave = IAaveInterface(_getAaveProvider().getLendingPool());
+    AaveDataProviderInterface aaveData = _getAaveDataProvider();
 
-    AaveInterface aave = AaveInterface(getAaveProvider().getLendingPool());
-    AaveDataProviderInterface aaveData = getAaveDataProvider();
+    bool isEth = _asset == _getEthAddr();
+    address _token = isEth ? _getWethAddr() : _asset;
 
-    bool isEth = collateralAsset == getEthAddr();
-    address _token = isEth ? getWethAddr() : collateralAsset;
-
-    TokenInterface tokenContract = TokenInterface(_token);
+    ITokenInterface tokenContract = ITokenInterface(_token);
 
     if (isEth) {
-      collateralAmount = collateralAmount == uint256(-1) ? address(this).balance : collateralAmount;
-      convertEthToWeth(isEth, tokenContract, collateralAmount);
+      _amount = _amount == uint256(-1) ? address(this).balance : _amount;
+      _convertEthToWeth(isEth, tokenContract, _amount);
+    } else {
+      _amount = _amount == uint256(-1) ? tokenContract.balanceOf(address(this)) : _amount;
     }
-    else {
-      collateralAmount = collateralAmount == uint256(-1) ? tokenContract.balanceOf(address(this)) : collateralAmount;
-    }
 
-    tokenContract.approve(address(aave), collateralAmount);
+    tokenContract.approve(address(aave), _amount);
 
-    aave.deposit(_token, collateralAmount, address(this), 0);
+    aave.deposit(_token, _amount, address(this), 0);
 
-    if (!getIsColl(aaveData, _token, address(this))) {
+    if (!_getIsColl(aaveData, _token, address(this))) {
       aave.setUserUseReserveAsCollateral(_token, true);
     }
   }
 
-      /**
-     * @dev Borrow ETH/ERC20_Token.
-     * @param borrowAsset token address to borrow.(For ETH: 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE)
-     * @param borrowAmount token amount to borrow.
-    */
-  function borrow(address borrowAsset, uint256 borrowAmount) external override payable {
+  /**
+   * @dev Borrow ETH/ERC20_Token.
+   * @param _asset token address to borrow.(For ETH: 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE)
+   * @param _amount token amount to borrow.
+   */
+  function borrow(address _asset, uint256 _amount) external payable override {
+    IAaveInterface aave = IAaveInterface(_getAaveProvider().getLendingPool());
 
-    AaveInterface aave = AaveInterface(getAaveProvider().getLendingPool());
+    bool isEth = _asset == _getEthAddr();
+    address _token = isEth ? _getWethAddr() : _asset;
 
-    bool isEth = borrowAsset == getEthAddr();
-    address _token = isEth ? getWethAddr() : borrowAsset;
-
-    aave.borrow(_token, borrowAmount, 2, 0, address(this));
-    convertWethToEth(isEth, TokenInterface(_token), borrowAmount);
+    aave.borrow(_token, _amount, 2, 0, address(this));
+    convertWethToEth(isEth, ITokenInterface(_token), _amount);
   }
 
-    /**
-     * @dev Withdraw ETH/ERC20_Token.
-     * @param collateralAsset token address to withdraw.
-     * @param collateralAmount token amount to withdraw.
-    */
-  function withdraw(address collateralAsset, uint256 collateralAmount) external override payable {
+  /**
+   * @dev Withdraw ETH/ERC20_Token.
+   * @param _asset token address to withdraw.
+   * @param _amount token amount to withdraw.
+   */
+  function withdraw(address _asset, uint256 _amount) external payable override {
+    IAaveInterface aave = IAaveInterface(_getAaveProvider().getLendingPool());
 
-    AaveInterface aave = AaveInterface(getAaveProvider().getLendingPool());
+    bool isEth = _asset == _getEthAddr();
+    address _token = isEth ? _getWethAddr() : _asset;
 
-    bool isEth = collateralAsset == getEthAddr();
-    address _token = isEth ? getWethAddr() : collateralAsset;
-
-    TokenInterface tokenContract = TokenInterface(_token);
+    ITokenInterface tokenContract = ITokenInterface(_token);
     uint256 initialBal = tokenContract.balanceOf(address(this));
 
-    aave.withdraw(_token, collateralAmount, address(this));
+    aave.withdraw(_token, _amount, address(this));
     uint256 finalBal = tokenContract.balanceOf(address(this));
-    collateralAmount = finalBal.sub(initialBal);
+    _amount = finalBal.sub(initialBal);
 
-    convertWethToEth(isEth, tokenContract, collateralAmount);
+    convertWethToEth(isEth, tokenContract, _amount);
   }
 
-      /**
-     * @dev Payback borrowed ETH/ERC20_Token.
-     * @param borrowAsset token address to payback.
-     * @param borrowAmount token amount to payback.
-    */
+  /**
+   * @dev Payback borrowed ETH/ERC20_Token.
+   * @param _asset token address to payback.
+   * @param _amount token amount to payback.
+   */
 
-  function payback(address borrowAsset, uint256 borrowAmount) external override payable {
+  function payback(address _asset, uint256 _amount) external payable override {
+    IAaveInterface aave = IAaveInterface(_getAaveProvider().getLendingPool());
+    AaveDataProviderInterface aaveData = _getAaveDataProvider();
 
-    AaveInterface aave = AaveInterface(getAaveProvider().getLendingPool());
-    AaveDataProviderInterface aaveData = getAaveDataProvider();
+    bool isEth = _asset == _getEthAddr();
+    address _token = isEth ? _getWethAddr() : _asset;
 
-    bool isEth = borrowAsset == getEthAddr();
-    address _token = isEth ? getWethAddr() : borrowAsset;
+    ITokenInterface tokenContract = ITokenInterface(_token);
 
-    TokenInterface tokenContract = TokenInterface(_token);
+    (, , uint256 variableDebt, , , , , , ) = aaveData.getUserReserveData(_token, address(this));
+    _amount = _amount == uint256(-1) ? variableDebt : _amount;
 
-    (,, uint variableDebt, , , , , , ) = aaveData.getUserReserveData(_token, address(this));
-    borrowAmount = borrowAmount == uint256(-1) ? variableDebt : borrowAmount;
+    if (isEth) _convertEthToWeth(isEth, tokenContract, _amount);
 
-    if (isEth) convertEthToWeth(isEth, tokenContract, borrowAmount);
+    tokenContract.approve(address(aave), _amount);
 
-    tokenContract.approve(address(aave), borrowAmount);
-
-    aave.repay(_token, borrowAmount, 2, address(this));
+    aave.repay(_token, _amount, 2, address(this));
   }
 }

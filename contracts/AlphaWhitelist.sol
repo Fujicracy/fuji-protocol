6073145a80bb31bca815ce6495795ee93dd6b289
Boyan Barakov <boyan.barakov@gmail.com>
2021-05-06 19:03:22 +0200

    lint AlphaWhitelist.sol

--- contracts/AlphaWhitelist.sol
+++ contracts/AlphaWhitelist.sol
@@ -5,107 +5,94 @@ import { IAlphaWhiteList } from "./IAlphaWhiteList.sol";
 import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
-import { Errors } from './Libraries/Errors.sol';
+import { Errors } from "./Libraries/Errors.sol";
 import { IFujiERC1155 } from "./FujiERC1155/IFujiERC1155.sol";
 
 import "hardhat/console.sol"; //test line
 
 contract AlphaWhitelist is IAlphaWhiteList, ReentrancyGuard, Ownable {
-
   using SafeMath for uint256;
 
-  uint256 public ETH_CAP_VALUE;
-  uint256 public LIMIT_USERS;
+  uint256 public ethCapValue;
+  uint256 public limitUsers;
   uint256 public counter;
 
   mapping(address => uint256) public whiteListed;
 
   // Log User entered
-	event userWhiteListed(address _userAddrs, uint256 _counter);
+  event UserWhitelisted(address _userAddrs, uint256 _counter);
 
   // Log Limit Users Changed
-  event userLimitUpdated(uint256 newUserLimit);
+  event UserLimitUpdated(uint256 newUserLimit);
 
   // Log Cap Value Changed
-  event capValueUpdated(uint256 newCapValue);
+  event CapValueUpdated(uint256 newCapValue);
 
   constructor(
     uint256 _limitUsers,
     uint256 _capValue,
     address _fliquidator
   ) public {
+    limitUsers = _limitUsers;
+    ethCapValue = _capValue;
 
-    LIMIT_USERS = _limitUsers;
-    ETH_CAP_VALUE = _capValue;
-
-    _addmetoWhiteList(_fliquidator);
+    _addMeToWhitelist(_fliquidator);
   }
 
-
   /**
-  * @dev Adds a user's address to the Fuji Whitelist
-  * Emits a {userWhiteListed} event.
-  */
-  function _addmetoWhiteList(address _usrAddrs) private nonReentrant {
+   * @dev Adds a user's address to the Fuji Whitelist
+   * Emits a {UserWhitelisted} event.
+   */
+  function _addMeToWhitelist(address _usrAddr) private nonReentrant {
+    require(whiteListed[_usrAddr] == 0, Errors.SP_ALPHA_WHITELIST);
+    require(counter <= limitUsers, Errors.SP_ALPHA_WHITELIST);
 
-    require(whiteListed[_usrAddrs] == 0, Errors.SP_ALPHA_WHITELIST);
-    require(counter <= LIMIT_USERS, Errors.SP_ALPHA_WHITELIST);
-
-    whiteListed[_usrAddrs] = counter;
+    whiteListed[_usrAddr] = counter;
     counter = counter.add(1);
 
-    emit userWhiteListed(_usrAddrs, counter);
+    emit UserWhitelisted(_usrAddr, counter);
   }
 
   /**
-  * @dev Checks if Address is in the Fuji Whitelist
-  * @param _usrAddrs: Address of the User to check
-  * @return True or False
-  */
-  function isAddrWhiteListed(address _usrAddrs) public view returns(bool) {
-    if (whiteListed[_usrAddrs] != 0) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  /**
-  * @dev Does Whitelist Routine to check if:
-  * - limit of users got reached
-  * - first deposit is less than ETH_CAP_VALUE
-  * - subsequent total deposits are less than 2*ETH_CAP_VALUE
-  * @param _usrAddrs: Address of the User to check
-  * @return letgo a boolean that allows a function to continue in "require" context
-  */
-  function whiteListRoutine(address _usrAddrs, uint64 _assetID, uint256 _amount, address _erc1155) external override returns(bool letgo) {
-    uint256 currentBalance = IFujiERC1155(_erc1155).balanceOf(_usrAddrs, _assetID);
+   * @dev Does Whitelist Routine to check if:
+   * - limit of users got reached
+   * - first deposit is less than ethCapValue
+   * - subsequent total deposits are less than 2*ethCapValue
+   * @param _usrAddr: Address of the User to check
+   * @return letgo a boolean that allows a function to continue in "require" context
+   */
+  function whiteListRoutine(
+    address _usrAddr,
+    uint64 _assetID,
+    uint256 _amount,
+    address _erc1155
+  ) external override returns (bool letgo) {
+    uint256 currentBalance = IFujiERC1155(_erc1155).balanceOf(_usrAddr, _assetID);
     if (currentBalance == 0) {
       counter = counter.add(1);
-      letgo = _amount <= ETH_CAP_VALUE && counter <= LIMIT_USERS;
+      letgo = _amount <= ethCapValue && counter <= limitUsers;
     } else {
-      letgo = currentBalance.add(_amount) <= ETH_CAP_VALUE.mul(2);
+      letgo = currentBalance.add(_amount) <= ethCapValue.mul(2);
     }
   }
 
   // Administrative Functions
 
   /**
-  * @dev Modifies the LIMIT_USERS
-  * @param _newUserLimit: New User Limint number
-  */
-  function modifyLimitUser(uint256 _newUserLimit) public onlyOwner {
-    LIMIT_USERS = _newUserLimit;
-    emit userLimitUpdated(_newUserLimit);
+   * @dev Modifies the limitUsers
+   * @param _newUserLimit: New User Limint number
+   */
+  function updateLimitUser(uint256 _newUserLimit) public onlyOwner {
+    limitUsers = _newUserLimit;
+    emit UserLimitUpdated(_newUserLimit);
   }
 
   /**
-  * @dev Modifies the ETH_CAP_VALUE
-  * @param _newEthCapValue: New ETH_CAP_VALUE
-  */
-    function modifyCap(uint256 _newEthCapValue) public onlyOwner {
-    ETH_CAP_VALUE = _newEthCapValue;
-    emit capValueUpdated(_newEthCapValue);
+   * @dev Modifies the ethCapValue
+   * @param _newEthCapValue: New ethCapValue
+   */
+  function updateCap(uint256 _newEthCapValue) public onlyOwner {
+    ethCapValue = _newEthCapValue;
+    emit CapValueUpdated(_newEthCapValue);
   }
-
 }

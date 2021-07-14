15f38098acb9c8b208dd4dc5741902fb6ec64247
Boyan Barakov <boyan.barakov@gmail.com>
2021-07-14 12:31:39 +0200

    move Compound common interfaces to separate file

--- contracts/Providers/ProviderFuse3.sol
+++ contracts/Providers/ProviderFuse3.sol
@@ -5,52 +5,7 @@ import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
 import { LibUniversalERC20 } from "../Libraries/LibUniversalERC20.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IProvider } from "./IProvider.sol";
-
-interface IGenCToken is IERC20 {
-  function redeem(uint256) external returns (uint256);
-
-  function redeemUnderlying(uint256) external returns (uint256);
-
-  function borrow(uint256 borrowAmount) external returns (uint256);
-
-  function exchangeRateCurrent() external returns (uint256);
-
-  function exchangeRateStored() external view returns (uint256);
-
-  function borrowRatePerBlock() external view returns (uint256);
-
-  function balanceOfUnderlying(address owner) external returns (uint256);
-
-  function borrowBalanceCurrent(address account) external returns (uint256);
-
-  function borrowBalanceStored(address account) external view returns (uint256);
-}
-
-interface ICErc20 is IGenCToken {
-  function mint(uint256) external returns (uint256);
-
-  function repayBorrow(uint256 repayAmount) external returns (uint256);
-
-  function repayBorrowBehalf(address borrower, uint256 repayAmount) external returns (uint256);
-}
-
-interface ICEth is IGenCToken {
-  function mint() external payable;
-
-  function repayBorrow() external payable;
-
-  function repayBorrowBehalf(address borrower) external payable;
-}
-
-interface IComptroller {
-  function markets(address) external returns (bool, uint256);
-
-  function enterMarkets(address[] calldata) external returns (uint256[] memory);
-
-  function exitMarket(address cTokenAddress) external returns (uint256);
-
-  function cTokensByUnderlying(address underlyingAsset) external view returns (address);
-}
+import { IGenCToken, ICErc20, ICEth, IComptroller } from "./ICompound.sol";
 
 contract HelperFunct {
   function _isETH(address token) internal pure returns (bool) {

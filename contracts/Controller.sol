043d61a0a34a347eab72c4e8faa67fcce301dbb4
icpigci <icpigci@gmail.com>
2021-07-16 03:23:55 +1000

    remove ABIEncoderV2

--- contracts/Controller.sol
+++ contracts/Controller.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
-pragma experimental ABIEncoderV2;
 
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { IVault } from "./Vaults/IVault.sol";

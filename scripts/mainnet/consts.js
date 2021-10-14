53ebc189ed202f485f8b835a439130969d20a786
Boyan Barakov <boyan.barakov@gmail.com>
2021-10-14 11:28:01 +0200

    move deploy scripts to own chain folder

---
+++ scripts/mainnet/consts.js
@@ -0,0 +1,31 @@
+const ASSETS = {
+  DAI: {
+    address: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+    oracle: "0xAed0c38402a5d19df6E4c03F4E2DceD6e29c1ee9",
+  },
+  USDC: {
+    address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+    oracle: "0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6",
+  },
+  USDT: {
+    address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+    oracle: "0x3E7d1eAB13ad0104d2750B8863b489D65364e32D",
+  },
+  ETH: {
+    address: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+    oracle: "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419",
+  },
+  FEI: {
+    name: "fei",
+    nameUp: "FEI",
+    address: "0x956F47F50A910163D8BF957Cf5846D573E7f87CA",
+    oracle: "0x31e0a88fecB6eC0a411DBe0e9E76391498296EE9",
+  },
+};
+
+const SUSHI_ROUTER_ADDR = "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F";
+
+module.exports = {
+  ASSETS,
+  SUSHI_ROUTER_ADDR,
+};

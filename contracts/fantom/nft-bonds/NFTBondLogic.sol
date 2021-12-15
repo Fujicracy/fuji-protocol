642dea2ded67d7fad10cba346004788fe16b1c37
DCota <32775237+DaigaroCota@users.noreply.github.com>
2021-12-15 16:17:33 -0500

    Update NFTBondLogic.sol

--- contracts/fantom/nft-bonds/NFTBondLogic.sol
+++ contracts/fantom/nft-bonds/NFTBondLogic.sol
@@ -23,9 +23,14 @@ contract NFTBondLogic is ERC1155 {
 
     mapping (address => UserData) public userdata;
 
+    address[] public validVaults;
+
     uint256 private MINIMUM_DAILY_DEBT_POSITION = 1; //tbd
     uint256 private POINT_PER_DEBTUNIT_PER_DAY = 1; //tbd
     uint256 private MULTIPLIER_RATE = 1; //tbd
+
+    constructor(string memory uri_) ERC1155(uri_) {
+    }
 
     // View Functions
 
@@ -33,7 +38,7 @@ contract NFTBondLogic is ERC1155 {
     * @notice Compute user's total debt in Fuji in all vaults of this chain.
     * @dev Must consider all fuji active vaults, and different decimals. 
     */
-    function balanceOf(address user, uint256 id) public view override returns(uint256){
+    function balanceOf(address user, uint256 id) public pure override returns(uint256){
         // To query points balance, id == 0
         if(id == 0 ) {
             return _pointsBalanceOf(user);
@@ -43,9 +48,9 @@ contract NFTBondLogic is ERC1155 {
     }
 
     /**
-    * @notice Compute user's accrued points since 'lastTimestampUpdate'.
+    * @notice Compute user's accrued points since user's 'lastTimestampUpdate'.
     */
-    function computeAccrued() public view returns(uint256) {
+    function computeAccrued() public pure returns(uint256) {
         return 1;
     }
 
@@ -53,19 +58,29 @@ contract NFTBondLogic is ERC1155 {
     * @notice Compute user's total debt in Fuji in all vaults of this chain.
     * @dev Must consider all fuji active vaults, and different decimals. 
     */
-    function getUserDebt() public view returns(uint256){
+    function getUserDebt() public pure returns(uint256){
         return 1;
     }
 
     // State Changing Functions
+    
+    /**
+    * @notice Compute user's total debt in Fuji in all vaults of this chain.
+    * @dev Must consider all fuji active vaults, and different decimals. 
+    */
+    function checkStateOfPoints() external {
+        // 1.- Check 'getUserDebt()'.
+        // 2.- 
+    }
 
     // Internal Functions
 
-    function _pointsBalanceOf(address user) internal view returns(uint256){
+    function _pointsBalanceOf(address user) internal pure returns(uint256){
         user;
         // 1.- Get user's accruedPoints
         // 2.- Get 'computeAccrued()'
         // 3.- add the values from 1 and 2 and return it.
+        return 1;
     }
 
     function _beforeTokenTransfer(
@@ -75,9 +90,10 @@ contract NFTBondLogic is ERC1155 {
         uint256[] memory ids,
         uint256[] memory amounts,
         bytes memory data
-    ) internal override {
+    ) internal pure override {
         operator;
         from;
+        to;
         ids;
         amounts;
         data;

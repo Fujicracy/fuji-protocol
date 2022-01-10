673a81db7f92e3b2f872632166b42fa6eaae9ac1
Tiago Neto <tiago.f.neto@gmail.com>
2022-01-10 11:20:28 +0000

    valid vault testing

--- test/fantom/nft-game/logic.js
+++ test/fantom/nft-game/logic.js
@@ -30,10 +30,20 @@ describe("Core Fuji Instance", function () {
 
   describe("NFT Bond Logic", function () {
     describe("Valid Vaults", function () {
-      it("Set valid vaults", async function () {
+      it("Set single valid vault", async function () {
         await expect(this.f.nftbondlogic.validVaults(0)).to.be.reverted;
         await this.f.nftbondlogic.setValidVaults([this.f.vaultftmdai.address]);
         expect(await this.f.nftbondlogic.validVaults(0)).to.be.equal(this.f.vaultftmdai.address);
+      });
+
+      it("Set multiple valid vaults", async function () {
+        await expect(this.f.nftbondlogic.validVaults(0)).to.be.reverted;
+        for (let i = 0; i < VAULTS.length; i++) {
+          console.log(await this.f.nftbondlogic.validVaults(i));
+          expect(await this.f.nftbondlogic.validVaults(i)).to.be.equal(
+            this.f[VAULTS[i].name].address
+          );
+        }
       });
     });
   });

c5d9e5828bdc7d13cd943b765e481922b459a5e2
Guillaume Lodi <guillaume.lodi@gmail.com>
2022-02-18 15:31:14 +0100

    fix: using the same format as other scripts

--- scripts/fantom/06_game_test_wallet.js
+++ scripts/fantom/06_game_test_wallet.js
@@ -1,13 +1,20 @@
-// Goal: initialize a chain w/ events to test the game
 require("dotenv").config();
+
+const chalk = require("chalk");
 const { ethers } = require("hardhat");
-const { getContractAddress, setDeploymentsPath } = require("../utils");
+const { getContractAddress, setDeploymentsPath, network } = require("../utils");
 
 const provider = ethers.providers.getDefaultProvider("http://localhost:8545");
 
 async function main() {
+  if (network !== "fantom") {
+    throw new Error("Please set 'NETWORK=fantom' in ./packages/hardhat/.env");
+  }
   await setDeploymentsPath("core");
+  await initTestWallet();
+}
 
+async function initTestWallet() {
   const amount = "20"; // 100 token in ganache-cli signer
 
   console.log("Sending tokens to test wallet...");
@@ -15,10 +22,14 @@ async function main() {
   console.log(`Sent ${ethers.utils.formatEther(tx.value)} tokens to ${tx.to}`);
 
   console.log("Borrowing w/ user account...");
-  const event = await depositAndBorrow(amount);
+  const event = await depositAndBorrow(String(amount - 1));
   console.log(`Borrowed ${ethers.utils.formatEther(event.value)} tokens`);
 
-  // 3. then forward time
+  console.log("Forwarding time...");
+  const seconds = 60 * 60 * 24 * 7;
+  await ethers.provider.send("evm_increaseTime", [seconds]);
+  await ethers.provider.send("evm_mine");
+  console.log(`Forwarded time ${seconds} (${seconds / 60 / 60 / 24} days) later`);
 }
 
 async function creditTestWallet(amount) {
@@ -50,6 +61,7 @@ async function depositAndBorrow(amount) {
     .depositAndBorrow(depositAmount, borrowAmount, { value: depositAmount });
 }
 
-main()
-  .catch(console.error)
-  .finally(() => console._ora.stop());
+main().catch((error) => {
+  console.error(chalk.red(`\n${error}\n`));
+  process.exit(1);
+});

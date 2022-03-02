c5f99f36e7486a3da59b972056dc9193813ab12c
DCota <32775237+DaigaroCota@users.noreply.github.com>
2022-03-02 14:37:35 -0500

    start of tests bonds

---
+++ test/fantom/nft-game/bond_functionality.js
@@ -0,0 +1,159 @@
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
+const { createFixtureLoader } = require("ethereum-waffle");
+
+const { WrapperBuilder } = require("redstone-evm-connector");
+
+const { BigNumber, provider } = ethers;
+
+const { fixture } = require("../utils");
+
+const { quickFixture, ASSETS, VAULTS } = require("./quick_test_fixture");
+
+const {
+  parseUnits,
+  formatUnitsToNum,
+  evmSnapshot,
+  evmRevert,
+  evmSetNextBlockTimestamp,
+  timeTravel,
+} = require("../../helpers");
+
+const FULL_FIXTURE = false;
+const DEBUG = true;
+
+if (FULL_FIXTURE) {
+  const { ASSETS, VAULTS } = require("../utils");
+} else {
+  const { ASSETS, VAULTS } = require("./quick_test_fixture");
+}
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const amountOfCratesToBuy = 10;
+
+describe("Bond Functionality", function () {
+  before(async function () {
+    this.users = await ethers.getSigners();
+
+    this.admin = this.users[0];
+    this.user = this.users[1];
+    this.otherUser = this.users[2];
+
+    const loadFixture = createFixtureLoader(this.users, provider);
+
+    if (FULL_FIXTURE) {
+      this.f = await loadFixture(fixture);
+    } else {
+      this.f = await loadFixture(quickFixture);
+    }
+
+    /// Specific testing conditions.
+
+    // Move to accumulation phase.
+    const now = (await provider.getBlock("latest")).timestamp;
+    const quicktimeGap = 2; // 2 seconds
+    const day = 60 * 60 * 24 ;
+    const phases = [
+      now,
+      now + quicktimeGap,
+      now + day,
+      now + day
+    ];
+    await this.f.nftgame.setGamePhases(phases);
+
+    // Force mint points for 'user'
+    await this.f.nftgame.mint(this.user.address, 0, parseUnits(200,5));
+
+    // Mint All cardIDs for 'user'
+    const cardIds = this.f.cardIds;
+    for (let index = cardIds[0]; index <= cardIds[1]; index++) {
+      await this.f.nftgame.mint(index, this.user.address);
+    }
+
+    // This condition is for testing only, allowing time travel and using redstone entropy feed.
+    await this.f.nftinteractions.setMaxEntropyDelay(60 * 60 * 24 * 365 * 2);
+
+    await timeTravel(day);
+
+    await this.f.nftinteractions.connect(this.user).lockFinalScore();
+
+    this.evmSnapshot0 = await evmSnapshot();
+  });
+
+  after(async function () {
+    evmRevert(this.evmSnapshot0);
+  });
+
+  describe("Basic Bond ERC721 Functionality", function () {
+
+    after(async function () {
+      evmRevert(this.evmSnapshot0);
+    });
+
+    it("Return value when calling 'balanceOf'", async function () {
+    
+    });
+
+    it("Revert when calling 'balanceOf' address 0", async function () {
+    
+    });
+
+    it("Returns owner of token ID when calling 'ownerOf'", async function () {
+    
+    });
+
+    it("Succesfully transfer tokenID", async function () {
+    
+    });
+
+    it("Returns the account approved for `tokenId` token", async function () {
+    
+    });
+
+    it("Succesfully transfer approved tokenID", async function () {
+    
+    });
+
+    it("Succesfully approve `operator` and performs transfer", async function () {
+    
+    });
+
+    it("Succesfully remove `operator` and reverts when trying transfer", async function () {
+    
+    });
+
+    it("", async function () {
+    
+    });
+
+    it("", async function () {
+    
+    });
+
+    it("", async function () {
+    
+    });
+
+    it("", async function () {
+    
+    });
+  });
+
+  describe("Basic Bond ERC3525 Functionality", function () {
+
+    after(async function () {
+      evmRevert(this.evmSnapshot0);
+    });
+
+    it("", async function () {
+    
+    });
+
+    it("", async function () {
+    
+    });
+
+  });
+
+
+});

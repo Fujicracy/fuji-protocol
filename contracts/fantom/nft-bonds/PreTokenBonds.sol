75f3a7f0d742d8066a5e2fe1cadd5ce546e27bcf
Tiago Neto <tiago.f.neto@gmail.com>
2022-03-09 21:21:27 +0000

    bonds setup

--- contracts/fantom/nft-bonds/PreTokenBonds.sol
+++ contracts/fantom/nft-bonds/PreTokenBonds.sol
@@ -1,16 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-import "./VoucherCore.sol";
+import "./utils/VoucherCore.sol";
+import "./NFTGame.sol";
 
 contract PreTokenBonds is VoucherCore {
+  /**
+   * @dev NFTGame contract address changed
+   */
+  event NFTGameChanged(address newAddress);
+
+  NFTGame private nftGame;
+
   function _initialize(
       string memory _name,
       string memory _symbol,
-      uint8 _unitDecimals
+      uint8 _unitDecimals,
+      address _nftGame
   ) internal override {
     VoucherCore._initialize(_name, _symbol, _unitDecimals);
+    nftGame = NFTGame(_nftGame);
   }
 
-  
+  /**
+   * @notice Set address for NFTGame contract
+   */
+  function setNFTGame(address _nftGame) external {
+    require(nftGame.hasRole(nftGame.GAME_ADMIN(), msg.sender), "No permission!");
+    nftGame = NFTGame(_nftGame);
+    emit NFTGameChanged(_nftGame);
+  }
+
+  /**
+   * @notice Function to be called from Interactions contract, after burning the points
+   */
+  function mint() {
+    require(nftGame.hasRole(nftGame.GAME_INTERACTOR(), msg.sender), "No permission!");
+  }
 }

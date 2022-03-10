4b0de169fabbafabf22e12ccf37f4aac7202d054
Tiago Neto <tiago.f.neto@gmail.com>
2022-03-10 17:47:52 +0000

    deposit underlying

--- contracts/fantom/nft-bonds/PreTokenBonds.sol
+++ contracts/fantom/nft-bonds/PreTokenBonds.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 import "./utils/VoucherCore.sol";
 import "./NFTGame.sol";
 
@@ -10,7 +12,14 @@ contract PreTokenBonds is VoucherCore {
    */
   event NFTGameChanged(address newAddress);
 
+  /**
+   * @dev Underlying token address changed
+   */
+  event UnderlyingChanged(address newAddress);
+
   NFTGame private nftGame;
+
+  address public underlying;
 
   function _initialize(
       string memory _name,
@@ -32,10 +41,27 @@ contract PreTokenBonds is VoucherCore {
   }
 
   /**
+   * @notice Set address for underlying Fuji ERC-20
+   */
+  function setNFTGame(address _underlying) external {
+    require(nftGame.hasRole(nftGame.GAME_ADMIN(), msg.sender), "No permission!");
+    underlying = _underlying;
+    emit UnderlyingChanged(_underlying);
+  }
+
+  /**
    * @notice Function to be called from Interactions contract, after burning the points
    */
   function mint(address _user, uint256 _slot, uint256 _units) external {
     require(nftGame.hasRole(nftGame.GAME_INTERACTOR(), msg.sender), "No permission!");
     _mint(_user, _slot, _units);
+  }
+
+  /**
+   * @notice Deposits Fuji ERC-20 tokens as underlying
+   */
+  function deposit(uint256 _amount) external {
+    require(nftGame.hasRole(nftGame.GAME_ADMIN(), msg.sender), "No permission!");
+    IERC20(underlying).transferFrom(msg.sender, address(this), _amount);
   }
 }

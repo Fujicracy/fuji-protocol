9bc5b3d85699360320ee402b7b29ec96875135c1
Tiago Neto <tiago.f.neto@gmail.com>
2022-03-16 15:41:31 +0000

    fixed access control

--- test/fantom/nft-game/access_control.js
+++ test/fantom/nft-game/access_control.js
@@ -38,15 +38,13 @@ describe("NFT Bond Logic", function () {
 
   describe("NFT Game", function () {
     it("Set valid vaults", async function () {
-      await expect(this.f.nftgame.connect(this.user).setValidVaults([])).to.be.revertedWith(
-        "No permission"
-      );
+      await expect(this.f.nftgame.connect(this.user).setValidVaults([])).to.be.revertedWith("111");
     });
 
     it("Set game phases", async function () {
       await expect(
         this.f.nftgame.connect(this.user).setGamePhases([0, 1, 2, 3])
-      ).to.be.revertedWith("No permission");
+      ).to.be.revertedWith("111");
     });
 
     it("Reverting state of points outside contract", async function () {
@@ -58,19 +56,19 @@ describe("NFT Bond Logic", function () {
     it("Mint", async function () {
       await expect(
         this.f.nftgame.connect(this.user).mint(this.user.address, 0, 1)
-      ).to.be.revertedWith("No permission");
+      ).to.be.revertedWith("111");
     });
 
     it("Burn", async function () {
       await expect(
         this.f.nftgame.connect(this.user).burn(this.user.address, 0, 1)
-      ).to.be.revertedWith("No permission");
+      ).to.be.revertedWith("111");
     });
 
     it("Set merkle root", async function () {
       const zeroBytes32 = "0x0000000000000000000000000000000000000000000000000000000000000000";
       await expect(this.f.nftgame.connect(this.user).setMerkleRoot(zeroBytes32)).to.be.revertedWith(
-        "No permission"
+        "111"
       );
     });
   });
@@ -79,25 +77,25 @@ describe("NFT Bond Logic", function () {
     it("Set NFT Game", async function () {
       await expect(
         this.f.nftinteractions.connect(this.user).setNFTGame(this.f.nftgame.address)
-      ).to.be.revertedWith("No permission");
+      ).to.be.revertedWith("111");
     });
 
     it("Set crate prices", async function () {
       await expect(
         this.f.nftinteractions.connect(this.user).setCratePrice(0, 0)
-      ).to.be.revertedWith("No permission");
+      ).to.be.revertedWith("111");
     });
 
     it("Set probability intervals", async function () {
       await expect(
         this.f.nftinteractions.connect(this.user).setProbabilityIntervals([])
-      ).to.be.revertedWith("No permission");
+      ).to.be.revertedWith("111");
     });
 
     it("Set crate rewards", async function () {
       await expect(
         this.f.nftinteractions.connect(this.user).setCrateRewards(0, [])
-      ).to.be.revertedWith("No permission");
+      ).to.be.revertedWith("111");
     });
   });
 });

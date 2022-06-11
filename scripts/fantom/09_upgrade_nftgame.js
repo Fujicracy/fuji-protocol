8c66848106faf0783cba3740eb20c9a22df47ee0
DCota <32775237+DaigaroCota@users.noreply.github.com>
2022-06-11 11:59:17 -0400

    added nftgame upgrade script

---
+++ scripts/fantom/09_upgrade_nftgame.js
@@ -0,0 +1,28 @@
+const chalk = require("chalk");
+const { defender } = require('hardhat');
+const { setDeploymentsPath, network, getContractAddress } = require("../utils");
+
+// ref: https://docs.openzeppelin.com/defender/guide-upgrades
+const upgrade = async () => {
+  const NFTGame = await ethers.getContractFactory("NFTGame");
+  const addr = getContractAddress("NFTGame");
+  console.log("Creating proposal for upgrading NFTGame...");
+  const proposal = await defender.proposeUpgrade(addr, NFTGame);
+  console.log("Upgrade proposal created at:", proposal.url);
+};
+
+const main = async () => {
+  if (network !== "fantom") {
+    throw new Error("Please set 'NETWORK=fantom' in ./packages/hardhat/.env");
+  }
+
+  await setDeploymentsPath("core");
+  await upgrade();
+};
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(chalk.red(`\n${error}\n`));
+    process.exit(1);
+  });

ad2c3ee3cd2fe59a03d80a68270185643975a19a
Tiago Neto <me@tiagofneto.com>
2022-06-24 16:37:54 +0100

    check new provider executeSwitch polygon

--- contracts/F2FujiVault.sol
+++ contracts/F2FujiVault.sol
@@ -278,6 +278,17 @@ contract F2FujiVault is VaultBaseUpgradeable, ReentrancyGuardUpgradeable, IVault
     uint256 _flashLoanAmount,
     uint256 _fee
   ) external payable override onlyFlash whenNotPaused {
+    // Check '_newProvider' is a valid provider
+    bool validProvider;
+    for (uint256 i = 0; i < providers.length; i++) {
+      if (_newProvider == providers[i]) {
+        validProvider = true;
+      }
+    }
+    if (!validProvider) {
+      revert(Errors.VL_INVALID_NEW_PROVIDER);
+    }
+
     // Compute Ratio of transfer before payback
     uint256 ratio = (_flashLoanAmount * 1e18) /
       (IProvider(activeProvider).getBorrowBalance(vAssets.borrowAsset));

eac7a1318c8d326f333a78d0a029da4235b86491
Tiago Neto <me@tiagofneto.com>
2022-06-27 21:27:18 +0100

    unwrapper

---
+++ contracts/WNativeUnwrapper.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../interfaces/IWETH.sol";
+
+contract WFTMUnwrapper {
+  address public immutable wNative;
+
+  constructor(_wNative) {
+    wNative = _wNative;
+  }
+
+  receive() external payable {}
+
+  /**
+   * @notice Convert WFTM to FTM and transfer to msg.sender
+   * @dev msg.sender needs to send WFTM before calling this withdraw
+   * @param _amount amount to withdraw.
+   */
+  function withdraw(uint256 _amount) external {
+    IWETH(wNative).withdraw(_amount);
+    (bool sent, ) = msg.sender.call{ value: _amount }("");
+    require(sent, "Failed to send native");
+  }
+}

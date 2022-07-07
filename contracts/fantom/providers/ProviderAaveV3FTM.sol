c70c94d2ea45a024606f91d8c8d301a67fbc3337
DCota <32775237+DaigaroCota@users.noreply.github.com>
2022-07-07 11:42:38 -0400

    fixed calling getBorrowRateFor()'

--- contracts/fantom/providers/ProviderAaveV3FTM.sol
+++ contracts/fantom/providers/ProviderAaveV3FTM.sol
@@ -42,7 +42,7 @@ contract ProviderAaveV3FTM is IProvider {
   function getBorrowRateFor(address _asset) external view override returns (uint256) {
     IAaveProtocolDataProvider aaveData = _getAaveProtocolDataProvider();
 
-    (, , , , uint256 variableBorrowRate, , , , , , , ) = aaveData.getReserveData(
+    (, , , , , , uint256 variableBorrowRate, , , , , ) = aaveData.getReserveData(
       _asset == _getNativeAddr() ? _getWrappedNativeAddr() : _asset
     );
 
